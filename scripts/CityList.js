105e6b6089f4e959b0c1e6d6748cfed467bd2d57
Seracrooce <fschmidt@live.com>
2021-11-23 10:20:36 -0600

    Completed Debugging Exercises

---
+++ scripts/CityList.js
@@ -0,0 +1,18 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ul>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li>${walker.city}</li>`
+    }
+
+    citiesHTML += "</ul>"
+
+    return citiesHTML
+}
+
+// citylist - 3
