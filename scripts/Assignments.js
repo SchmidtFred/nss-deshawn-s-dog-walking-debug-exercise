5a6c8fff0484d90713edc3e436c407ef4c3ea38f
Seracrooce <fschmidt@live.com>
2021-11-30 10:33:25 -0600

    Added city database and refactored other code accordingly

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,9 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const cities = getCities();
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -18,16 +19,21 @@ const findPetWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+const findWalkerCity = (walker, allCities) => {
+    return allCities.find(item => item.id === walker.cityId);
+}
+
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
-        const currentPetWalker = findPetWalker(currentPet, walkers)
+        const currentPetWalker = findPetWalker(currentPet, walkers);
+        const currentCity = findWalkerCity(currentPetWalker, cities);
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentCity.name}
             </li>
         `
     }
